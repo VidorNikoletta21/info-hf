37b28a19da9e25590237e4607d80d85f9551588f
VidorNikoletta21 <nikolettavi21@gmail.com>
2024-10-13 15:31:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-document.getElementById('get-recipe').addEventListener('click', async () => {
-    try {
-        const response = await fetch('www.themealdb.com/api/json/v1/1/categories.php');
-        const data = await response.json();
-        const recipe = data.recipes[0];
-        
-        document.getElementById('recipe').innerHTML = `
-            <h2>${recipe.title}</h2>
-            <img src="${recipe.image}" alt="${recipe.title}">
-            <p>${recipe.instructions}</p>
-        `;
-    } catch (error) {
-        console.error('Hiba történt:', error);
+
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
     }
-});
+  });
